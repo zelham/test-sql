50d3f55d718f93126aeb6e3aecdc039f8d29d5c5
BihtSift <bihtsift@gmail.com>
2020-08-06 15:36:45 -0700

    Added to practice

--- src/GameTest/Practice/Program.cs
+++ src/GameTest/Practice/Program.cs
@@ -15,22 +15,37 @@ namespace Practice
             \**************/
             // Practice 1: Write a function that outputs text
 
+
             // Practice 2: Write a function that takes a string and outputs it
+
 
             // Practice 3: Write a function that takes two integers, adds them together and outputs the result
 
+
             // Practice 4: Write a function that takes two integers and returns the result
 
+
             // Practice 5: Write a function that takes two strings, concatenates them and returns a new string
+
 
             /*********\
             |* Logic *|
             \*********/
-            // Practice 6: Write a function that compares two numbers and returns the bigger one
+            // Practice 1: Write a function that compares two integers and returns the bigger one
 
-            // Practice 7: Write a function that compares two numbers and returns true if they are equal and false if not
 
-            // Practice 8: Write a function that 
+            // Practice 2: Write a function that compares two integers are returns the smaller one
+
+
+            // Practice 3: Write a function that compares two integers and returns true if they are equal and false if not
+
+
+            /**************\
+            |* While Loop *|
+            \**************/
+            // Practice 1: Write a function that outputs the same text 10 times
+
+            // Practice 2: Write a function that takes an integer and outputs the same text the number of times provided
         }
     }
 }
