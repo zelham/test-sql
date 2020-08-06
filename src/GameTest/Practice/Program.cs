2f170a38bb19133eefbea7064a032b2d5a9e290e
BihtSift <bihtsift@gmail.com>
2020-08-06 15:12:14 -0700

    Added Practice project

---
+++ src/GameTest/Practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**************\
+            |* Functions  *|
+            \**************/
+            // Practice 1: Write a function that outputs text
+
+            // Practice 2: Write a function that takes a string and outputs it
+
+            // Practice 3: Write a function that takes two integers, adds them together and outputs the result
+
+            // Practice 4: Write a function that takes two integers and returns the result
+
+            // Practice 5: Write a function that takes two strings, concatenates them and returns a new string
+
+            /*********\
+            |* Logic *|
+            \*********/
+            // Practice 6: Write a function that compares two numbers and returns the bigger one
+
+            // Practice 7: Write a function that compares two numbers and returns true if they are equal and false if not
+
+            // Practice 8: Write a function that 
+        }
+    }
+}
